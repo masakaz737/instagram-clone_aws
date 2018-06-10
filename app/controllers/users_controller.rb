ff20b3f84eaa1c75d46d53e74a7b9d4eeb68b71f
ishida_masakazu <u225210b@gmail.com>
2018-06-11 08:40:40 +0900

    add mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :login_or_not, {only: [:show, :favorites]}
+  before_action :ensure_correct_user, {only: [:show, :favorites]}
+
   def new
     @user = User.new
   end
@@ -26,4 +29,17 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
+  def login_or_not
+    if !current_user
+      redirect_to new_session_path, notice: "ログインしてください"
+    end
+  end
+
+  def ensure_correct_user
+    if current_user.id != params[:id].to_i
+      redirect_to posts_path, notice: "権限がありません"
+    end
+  end
+
 end
